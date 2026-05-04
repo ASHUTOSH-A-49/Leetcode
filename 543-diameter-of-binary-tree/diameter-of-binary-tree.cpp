8ce643899d1059a92c0e9b56640ffe64353d8d5e
Ashutosh Behera <ashupoco66@gmail.com>
2026-05-04 12:43:19 +0530

    Time: 223 ms (18.14%) | Memory: 23.9 MB (12.07%) - LeetSync

---
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+// diameter = left deepest node height + right deepest node height 
+int ans = 0;
+int depth(TreeNode* Node){
+    if(Node->left==NULL && Node->right==NULL){
+        return 1;
+    }
+    if(Node->left==NULL){
+        return 1+depth(Node->right);
+    }
+    if(Node->right==NULL){
+        return 1+depth(Node->left);
+    }
+    return 1+ max(depth(Node->left),depth(Node->right));
+
+}
+int getDia(TreeNode* root){
+    int l,r;
+    (root->left==NULL)?(l = 0) : l = depth(root->left); 
+    (root->right==NULL)?(r = 0) : r = depth(root->right); 
+    return l+r;
+}
+void inorder(TreeNode * node){
+    if(node==NULL) return;
+    
+    inorder(node->left);
+    ans = max(ans,getDia(node));
+    inorder(node->right);
+}
+    int diameterOfBinaryTree(TreeNode* root) {
+        // if(root->left==NULL && root->right==NULL) return 1;
+        inorder(root);    
+        return ans;
+    }
+};
