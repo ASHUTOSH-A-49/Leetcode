5ddf06e80b0c396d1f043088af3f1d1765d5be89
Ashutosh Behera <ashupoco66@gmail.com>
2026-05-07 20:41:18 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (25.70%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.cpp
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -13,50 +13,13 @@ class Solution {
 public:
 
 // iterative -level order solution with 2 vectors
-vector<vector<int>> nodes;
-void level(queue <TreeNode*> & q){
-    while(!q.empty()){
-        int n = q.size();
-        vector<int> level;
-        for(int i = 0;i<n;i++){
-            TreeNode *Node = q.front();
-            q.pop();
-            if(Node!=NULL){
-                q.push(Node->left);
-                q.push(Node->right);
-                level.push_back(Node->val); 
-            }else{
-                level.push_back(-101);
-            }
-            
-        }
-        nodes.push_back(level);
-        
-    }
+bool solve(TreeNode* p,TreeNode*q ){
+    if(p==NULL && q==NULL) return true;
+    if(p==NULL || q==NULL) return false;
+
+    return (p->val==q->val) && (solve(p->left,q->right)) && (solve(p->right,q->left));
 }
     bool isSymmetric(TreeNode* root) {
-        vector<int> rightNodes;
-        vector<int> leftNodes;
-        queue<TreeNode*>q;
-        q.push(root);
-        level(q);
-        for(vector<int> v: nodes){
-            for(int i = 0;i<v.size();i++){
-                leftNodes.push_back(v[i]);
-            }
-        }
-        for(vector<int> v: nodes){
-            for(int i = v.size()-1;i>=0;i--){
-                rightNodes.push_back(v[i]);
-            }
-        }
-        for(int i:leftNodes) cout<<i<<" ";
-        cout<<endl;
-        for(int i:rightNodes) cout<<i<<" ";
-        cout<<endl;
-        for(int i = 0;i<leftNodes.size();i++){
-            if(leftNodes[i]!=rightNodes[i]) return false;
-        }    
-        return true;
+        return solve(root->left,root->right);
     }
 };
