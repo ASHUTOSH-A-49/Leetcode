fde5a40c2d4f7e9878da933050dbcc77586851f4
Ashutosh Behera <ashupoco66@gmail.com>
2026-05-31 11:57:09 +0530

    Time: 44 ms (7.15%) | Memory: 106.8 MB (16.20%) - LeetSync

--- 2245-destroying-asteroids/destroying-asteroids.cpp
+++ 2245-destroying-asteroids/destroying-asteroids.cpp
@@ -1,44 +1,12 @@
 class Solution {
 public:
     bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
-        long long cmass = mass;
-        asteroids.push_back(mass);
+        long long newmass= mass;
         sort(asteroids.begin(),asteroids.end());
-        vector<int> smol,big;
-        int idx;
-        for(int i = 0;i<asteroids.size();i++){
-            if(asteroids[i]==mass){
-                while(i<asteroids.size() && asteroids[i]==mass){
-                    i++;
-                }
-                idx = i-1;
-                break;
-            }
+        for(int i:asteroids){
+            if(newmass<i) return false;
+            newmass+=i;
         }
-        for(int i = 0;i<idx;i++){
-            smol.push_back(asteroids[i]);
-        }
-        for(int i = idx+1;i<asteroids.size();i++){
-            big.push_back(asteroids[i]);
-        }
-
-        while(big.size()>0){
-            long long newmass = cmass;
-            
-            while(newmass<big[0]){
-                if(smol.size()==0) break;
-                newmass+= smol[smol.size()-1];
-                int remidx = smol.size()-1;
-                smol.erase(smol.begin()+remidx);
-            }
-            if(big.size()==0) break;
-            if(newmass<big[0]) return false;
-            newmass+=big[0];
-            big.erase(big.begin());
-            cmass = newmass;
-        }
-
-        if(big.size()>0) return false;
         return true;
 
     }
