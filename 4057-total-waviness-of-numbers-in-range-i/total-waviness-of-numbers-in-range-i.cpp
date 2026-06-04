a86226b499641316fe4a9354183c3d07908ed3cc
Ashutosh Behera <ashupoco66@gmail.com>
2026-06-04 13:14:49 +0530

    Time: 7 ms (88.81%) | Memory: 8.6 MB (99.07%) - LeetSync

--- 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.cpp
+++ 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.cpp
@@ -2,17 +2,22 @@ class Solution {
 public:
 int cntwavy(int n){
     if((n/100)==0) return 0;
-    vector<int> digs;
-    while(n){
-        int dig = n%10;
-        digs.push_back(dig); 
-        n/=10;
-    }
-    int siz = digs.size();
     int cnt = 0;
-    for(int i = 1;i<siz-1;i++){
-        if((digs[i]>digs[i-1] && digs[i]>digs[i+1]) || (digs[i]<digs[i-1] && digs[i]<digs[i+1])) cnt++;
+    // vector<int> digs;
+    int prevdig = n%10;
+    n = n/10;
+    while(n/10!=0){
+        int dig = n%10;
+        n/=10;
+        int nextdig = n%10;
+        if((dig<nextdig && dig<prevdig) || (dig>nextdig && dig>prevdig)) cnt++;
+        prevdig = dig;
     }
+    // int siz = digs.size();
+    // int cnt = 0;
+    // for(int i = 1;i<siz-1;i++){
+    //     if((digs[i]>digs[i-1] && digs[i]>digs[i+1]) || (digs[i]<digs[i-1] && digs[i]<digs[i+1])) cnt++;
+    // }
     return cnt;
 }
     int totalWaviness(int num1, int num2) {
