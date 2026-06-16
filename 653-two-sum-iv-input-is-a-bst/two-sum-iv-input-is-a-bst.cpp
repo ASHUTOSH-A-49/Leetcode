b90ee44776f170eef00dccb0c71e3ba7ce809f73
Ashutosh Behera <ashupoco66@gmail.com>
2026-06-16 19:16:04 +0530

    Time: 15 ms (9.55%) | Memory: 38.9 MB (29.45%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
@@ -11,32 +11,15 @@
  */
 class Solution {
 public:
-    void traverse(TreeNode* root,vector<int> & nodes){
-        if(!root) return;
-        traverse(root->left,nodes);
-        nodes.push_back(root->val);
-        traverse(root->right,nodes);
-    }
-    bool BSFind(vector<int> nodes,int target,int i){
-        
-        int low = i,high = nodes.size()-1;
-        while(low<=high){
-            int mid = low + (high-low)/2;
-            if(nodes[mid]==target) return true;
-            else if(nodes[mid]<target) low = mid+1;
-            else high = mid-1;
-        }
-        return false;
+//set approach
+    bool dfs(TreeNode* root,int k,unordered_set<int> &seen){
+        if (!root) return false;
+        if (seen.count(k - root->val)) return true;
+        seen.insert(root->val);
+        return dfs(root->left, k, seen) || dfs(root->right, k, seen);
     }
     bool findTarget(TreeNode* root, int k) {
-        vector<int> nodes;
-        traverse(root,nodes);
-        int n = nodes.size();
-        for(int i:nodes) cout<<i<<" ";
-        cout<<endl;
-        for(int i = 0;i<n;i++){
-            if(BSFind(nodes,k-nodes[i],i+1)) return true;
-        }
-        return false;
+        unordered_set<int> seen;
+        return dfs(root, k, seen);
     }
 };
