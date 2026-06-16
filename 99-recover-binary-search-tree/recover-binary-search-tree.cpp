b49d13f4ada9122e04b6874607a55a5ea6e2be77
Ashutosh Behera <ashupoco66@gmail.com>
2026-06-16 20:12:59 +0530

    Time: 3 ms (18.53%) | Memory: 61.3 MB (7.47%) - LeetSync

---
+++ 99-recover-binary-search-tree/recover-binary-search-tree.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+void inorderTrav(TreeNode* root,vector<TreeNode*> & nodes){
+    if(!root) return;
+    inorderTrav(root->left,nodes);
+    nodes.push_back(root);
+    inorderTrav(root->right,nodes);
+}
+    void recoverTree(TreeNode* root) {
+        vector<TreeNode*> nodes;
+        inorderTrav(root,nodes);
+        TreeNode* n1 = nullptr, *n2 = nullptr;
+        int ind = nodes.size()-1;
+        for(int i  = 0;i<ind;i++){
+            if(nodes[i]->val > nodes[i+1]->val){
+                if(n1==nullptr){
+                    n1 = nodes[i];
+                    n2 = nodes[i + 1]; 
+                }
+                else n2 = nodes[i+1];
+            }
+        }
+        if (n1 && n2) {
+            int temp = n2->val;
+            n2->val = n1->val;
+            n1->val = temp;
+        }
+
+    }
+};
