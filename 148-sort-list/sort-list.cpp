adc142d3c79d32beb14905f21afd5a4e3471fb8a
Ashutosh Behera <ashupoco66@gmail.com>
2026-06-29 12:26:33 +0530

    Time: 61 ms (8.54%) | Memory: 71.8 MB (44.18%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -13,16 +13,14 @@ class Solution {
 public:
     // approach 2-array then sort or set  TC - O(NlogN), SC-O(N);
     ListNode* sortList(ListNode* head) {
-        vector<int> arr;
+        multiset<int> nodes;
         ListNode*tmp1 = head,*tmp2 = head;
         while(tmp1){
-            arr.push_back(tmp1->val);
+            nodes.insert(tmp1->val);
             tmp1= tmp1->next;
         }
-        sort(arr.begin(),arr.end());
-        int i = 0;
-        while(tmp2){
-            tmp2->val = arr[i++];
+        for(int i:nodes){
+            tmp2->val = i;
             tmp2 = tmp2->next;
         }
         return head;
