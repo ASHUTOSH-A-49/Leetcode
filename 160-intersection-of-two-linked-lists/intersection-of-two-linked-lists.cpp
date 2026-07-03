bae36299d9862f00d5db851731246a9d8400e4e7
Ashutosh Behera <ashupoco66@gmail.com>
2026-07-03 12:04:08 +0530

    Time: 59 ms (41.80%) | Memory: 23.8 MB (86.11%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -8,16 +8,30 @@
  */
 class Solution {
 public:
-//recursive soln;
-
+//count nodes  and then start from same length;
+    int cntNodes(ListNode * head){
+        int cnt = 0;
+        while(head){
+            head = head->next;
+            cnt++;
+        }
+        return cnt;
+    }
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         if (!headA || !headB) return nullptr;
-        ListNode *ptrA = headA;
-        ListNode *ptrB = headB;
-        while (ptrA != ptrB) {
-            ptrA = (ptrA == nullptr) ? headB : ptrA->next;
-            ptrB = (ptrB == nullptr) ? headA : ptrB->next;
+        int n1 = cntNodes(headA),n2 = cntNodes(headB);
+        while(n1>n2){
+            n1--;
+            headA = headA->next;
         }
-        return ptrA;
+        while(n2>n1){
+            n2--;
+            headB = headB->next;
+        }
+        while(headA && headB){
+            if(headA==headB) return headA;
+            headA = headA->next;headB = headB->next;
+        }
+        return nullptr;
     }
 };
