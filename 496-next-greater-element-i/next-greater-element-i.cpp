8dbeab55d1f734e482ac9e9f3fbf5e4fdaf9b533
Ashutosh Behera <ashupoco66@gmail.com>
2026-07-10 13:14:24 +0530

    Time: 3 ms (40.94%) | Memory: 13 MB (15.66%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-//APPROACH -1 : 2 stack approach
+//APPROACH -2:monotonous stack+ hash
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         vector<int> ans;
-        stack<int> st1,st2;
-        for(int i:nums2) st1.push(i);
+        map<int,int> NGE; //as says distinct int so we can use map
+        stack<int> st;
+        int n = nums2.size();
+        for(int i = n-1;i>=0;i--){
+            //maintain monotonocity
+            while(!(st.empty())&& st.top()<nums2[i]) st.pop();
+            if(st.empty()) NGE[nums2[i]] = -1;
+            else NGE[nums2[i]] = st.top();
+            st.push(nums2[i]); //psh to maintain NGE and monotonicity logic together
+        }
         for(int i:nums1){
-            int nextgr = -1;
-            while(st1.top()!=i){
-                if(st1.top()>i){
-                    nextgr = st1.top();
-                }
-                st2.push(st1.top());
-                st1.pop();
-            }
-            while(!st2.empty()){
-                st1.push(st2.top());
-                st2.pop();
-            }
-            ans.push_back(nextgr);
+            ans.push_back(NGE[i]);
         }
         return ans;
     }
