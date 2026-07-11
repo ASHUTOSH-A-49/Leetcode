62180f8d1f6aa397e8aad9ca08d233a601067b9e
Ashutosh Behera <ashupoco66@gmail.com>
2026-07-11 21:30:45 +0530

    Time: 150 ms (5.03%) | Memory: 217.3 MB (5.11%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-int MOD = 1e9+7;
-int maxi  = 1e6;
+int MOD = 1e9 + 7;
     vector<pair<int,int>> getPSE(vector<int>& nums) {
         int n = nums.size();
         stack<pair<int,int>> st;
         vector<pair<int,int>> PSE(n);
-        for(int i = 0;i<n;i++){
-            while(!(st.empty())&& st.top().first>nums[i]) st.pop();
-            if(st.empty()) PSE[i] = {maxi,0};
+        for(int i = 0; i < n; i++){
+            while(!(st.empty()) && st.top().first > nums[i]) 
+                st.pop();
+            if(st.empty()) PSE[i] = {-1, -1}; 
             else PSE[i] = st.top();
-            st.push({nums[i],i+1}); //psh to maintain NSE and monotonicity logic together
+            st.push({nums[i], i}); 
         }
         return PSE;
     }
@@ -18,29 +18,27 @@ int maxi  = 1e6;
         int n = nums.size();
         stack<pair<int,int>> st;
         vector<pair<int,int>> NSE(n);
-        for(int i = n-1;i>=0;i--){
-            while(!(st.empty())&& st.top().first>=nums[i]) st.pop();
-            if(st.empty()) NSE[i] = {maxi,n};
+        for(int i = n - 1; i >= 0; i--){
+            while(!(st.empty()) && st.top().first >= nums[i]) 
+                st.pop();
+            if(st.empty()) NSE[i] = {-1, n}; 
             else NSE[i] = st.top();
-            st.push({nums[i],i}); //psh to maintain NSE and monotonicity logic together
+            st.push({nums[i], i}); 
         }
         return NSE;
     }
     int sumSubarrayMins(vector<int>& arr) {
-        vector<pair<int,int>> PSE,NSE;
-        PSE = getPSE(arr);
-        NSE = getNSE(arr);
-        int n= arr.size();
+        vector<pair<int,int>> PSE = getPSE(arr);
+        vector<pair<int,int>> NSE = getNSE(arr);
+        int n = arr.size();
         int ans = 0;
-        for(int i = 0;i<n;i++){
-            auto ps = PSE[i],ns = NSE[i];
-            int l = i-ps.second+1;//left number till PSE idx
-            int r = ns.second-i;//left number till NSE idx
-            // cout<<l<<" "<<r<<endl;
-            long long sub  = l*r; //number of subarrays in which num at idx i will be minimum
-            long long res = (sub*arr[i])%MOD;
-            // cout<<res<<endl;
-            ans=(ans+res)%MOD;
+        for(int i = 0; i < n; i++){
+            auto ps = PSE[i], ns = NSE[i];
+            long long l = i - ps.second; 
+            long long r = ns.second - i; 
+            long long sub = (l * r) % MOD; 
+            long long res = (sub * arr[i]) % MOD;
+            ans = (ans + res) % MOD;
         }
         return ans;
     }
