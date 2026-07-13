e3305d3703e80c15807f3b18f3f7c8ddc7d196b9
Ashutosh Behera <ashupoco66@gmail.com>
2026-07-13 18:46:16 +0530

    Time: 35 ms (17.75%) | Memory: 83.8 MB (54.16%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,43 +1,31 @@
 class Solution {
 public:
-//approach  - 1 : use PSE and NSE and then get to every bar and get its rectangle size
-    vector<pair<int,int>> getPSE(vector<int>& nums) {
-        int n = nums.size();
-        stack<pair<int,int>> st;
-        vector<pair<int,int>> PSE(n);
-        for(int i = 0; i < n; i++){
-            while(!(st.empty()) && st.top().first > nums[i]) 
-                st.pop();
-            if(st.empty()) PSE[i] = {-1, -1}; 
-            else PSE[i] = st.top();
-            st.push({nums[i], i}); 
-        }
-        return PSE;
-    }
-    vector<pair<int,int>> getNSE(vector<int>& nums) {
-        int n = nums.size();
-        stack<pair<int,int>> st;
-        vector<pair<int,int>> NSE(n);
-        for(int i = n - 1; i >= 0; i--){
-            while(!(st.empty()) && st.top().first >= nums[i]) 
-                st.pop();
-            if(st.empty()) NSE[i] = {-1, n}; 
-            else NSE[i] = st.top();
-            st.push({nums[i], i}); 
-        }
-        return NSE;
-    }
+//approach  - 2: greedily use monotonic stack to get NSE and PSE
+//must watch video to undertand the approach and intuition for this
+    
     int largestRectangleArea(vector<int>& heights) {
-        vector<pair<int,int>> PSE = getPSE(heights);
-        vector<pair<int,int>> NSE = getNSE(heights);
-
+        stack<pair<int,int>> st;
         int n=  heights.size(),maxi = 0;
         for(int i = 0;i<n;i++){
             int h = heights[i];
-            int pseidx = PSE[i].second,nseidx = NSE[i].second;
-            // (nseidx-1)-(pseidx+1) +1
-            int b = nseidx-pseidx-1;
-            maxi = max(maxi,h*b);
+            int nse  = i;
+            while(!st.empty() && st.top().first>h){
+                auto p  = st.top();
+                st.pop();
+                int pse = (st.empty()) ?-1 : st.top().second;
+                int ar = (nse-pse-1)*p.first;
+                maxi = max(maxi,ar);
+                
+            }
+            st.push({h,i});
+        }
+        int nse = n;
+        while(!st.empty()){
+            auto p = st.top();
+            st.pop();
+            int pse = (st.empty()) ?-1 : st.top().second;
+            int ar = (nse-pse-1)*p.first;
+            maxi = max(maxi,ar);
         }
         return maxi;
     }
