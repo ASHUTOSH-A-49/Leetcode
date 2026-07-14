0d724674fee8c8ff52043a3ceece5d80c681a493
Ashutosh Behera <ashupoco66@gmail.com>
2026-07-14 16:37:01 +0530

    Time: 923 ms (15.80%) | Memory: 290.1 MB (14.77%) - LeetSync

--- 3181-find-building-where-alice-and-bob-can-meet/find-building-where-alice-and-bob-can-meet.cpp
+++ 3181-find-building-where-alice-and-bob-can-meet/find-building-where-alice-and-bob-can-meet.cpp
@@ -18,7 +18,7 @@ class SegTree{
          
     }
 
-    int RangeMaxQuery(int idx, int start, int end, int l, int r, vector<int> & arr) { 
+    int RangeMaxIDXQuery(int idx, int start, int end, int l, int r, vector<int> & arr) { 
         if (r < start || end < l) { 
             return -1; 
         } 
@@ -26,8 +26,8 @@ class SegTree{
             return Stree[idx];
         } 
         int mid = (l + r) / 2; 
-        int left = RangeMaxQuery(2 * idx + 1, start, end, l, mid, arr);
-        int right = RangeMaxQuery(2 * idx + 2, start, end, mid + 1, r, arr);
+        int left = RangeMaxIDXQuery(2 * idx + 1, start, end, l, mid, arr);
+        int right = RangeMaxIDXQuery(2 * idx + 2, start, end, mid + 1, r, arr);
 
         if (left == -1) return right;
         if (right == -1) return left;
@@ -41,26 +41,28 @@ class SegTree{
         Stree.assign(4*n,0);
         if(n>0) BuildTree(nums,0,0,n-1);
     }
-    int getLeftMostValid(int idx, int start, int end, int l, int r, int target, vector<int> & arr) {
-        if (r < start || end < l || arr[Stree[idx]] <= target) {
-            return -1; 
+    int getLeftMostBS(int start, int end, int l, int r, int target, vector<int> & arr) {
+        int low = start,high= end,ans = -1;
+        while(low<=high){
+            int mid = low+ (high-low)/2;
+            int idx = RangeMaxIDXQuery(0,low,mid,0,n-1,arr);
+            if(idx!=-1 && arr[idx]>target){
+                ans = idx;
+                high = mid-1;
+            }else low = mid+1;
         }
-        if (l == r) {
-            return l;
-        }
-        int mid = (l + r) / 2;
-        int left = getLeftMostValid(2 * idx + 1, start, end, l, mid, target, arr);
-        if (left != -1) return left;
-        return getLeftMostValid(2 * idx + 2, start, end, mid + 1, r, target, arr);
+        return ans;
+
     }
     int query(int start,int end,int target,vector<int> & arr){
         // a and b are alice and bob indexes
-        return getLeftMostValid(0,start,end,0,n-1,target,arr);
+        return getLeftMostBS(start,end,0,n-1,target,arr);
     }
 };
 
 class Solution {
 public:
+//approach -  RMIQ + BS
     vector<int> leftmostBuildingQueries(vector<int>& heights, vector<vector<int>>& queries) {
         SegTree st(heights);
         vector<int> ans;
@@ -69,9 +71,11 @@ public:
             int a = q[0],b = q[1];
             if (a > b) swap(a, b); 
             if (a == b || heights[a] < heights[b]) { 
+                //case I and edge case handled here only
                 ans.push_back(b); 
                 continue; 
             }
+            //when a<b, and heights[a] > heights[b] ... use seg treee
             int idx = st.query(b+1,n-1,heights[a],heights);
             ans.push_back(idx);
         }
