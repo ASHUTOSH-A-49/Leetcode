e55a52f5840904dd326034a88e1811ecfdf5304f
Ashutosh Behera <ashupoco66@gmail.com>
2026-07-19 23:18:34 +0530

    Time: 68 ms (87.38%) | Memory: 112.9 MB (59.98%) - LeetSync

---
+++ 1876-map-of-highest-peak/map-of-highest-peak.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
+        int m = isWater.size(), n = isWater[0].size();
+        queue<pair<int, int>> q;
+        vector<vector<int>> ans(m, vector<int>(n, -1));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                isWater[i][j] = !isWater[i][j];
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (isWater[i][j] == 0) {
+                    ans[i][j] = 0;
+                    q.push({i, j});
+                }
+            }
+        }
+        while (!q.empty()) {
+            auto [r, c] = q.front();
+            q.pop();
+            if (r + 1 < m && ans[r + 1][c] == -1) { ans[r + 1][c] = ans[r][c] + 1; q.push({r + 1, c}); }
+            if (r - 1 >= 0 && ans[r - 1][c] == -1) { ans[r - 1][c] = ans[r][c] + 1; q.push({r - 1, c}); }
+            if (c + 1 < n && ans[r][c + 1] == -1) { ans[r][c + 1] = ans[r][c] + 1; q.push({r, c + 1}); }
+            if (c - 1 >= 0 && ans[r][c - 1] == -1) { ans[r][c - 1] = ans[r][c] + 1; q.push({r, c - 1}); }
+        }
+        return ans;
+    }
+};
