5167669228ac04dcf554c0c9ea1cf4d657176090
Ashutosh Behera <ashupoco66@gmail.com>
2026-07-24 16:10:01 +0530

    Time: 12 ms (30.30%) | Memory: 36.2 MB (38.23%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,21 +1,34 @@
 class Solution {
 public:
-int DFS(vector<vector<int>> & grid,vector<vector<int>> & visited,int i,int j){
-    if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size()) return 0;
-    if(visited[i][j]) return 0;
-    if(grid[i][j]==0) return 0;
-    visited[i][j] = 1;
-    return 1 + (
-        DFS(grid,visited,i,j+1)+
-        DFS(grid,visited,i,j-1)+
-        DFS(grid,visited,i+1,j)+
-        DFS(grid,visited,i-1,j)
-    );
+// BFS SOLUTION 
+int BFS(vector<vector<int>> & grid,vector<vector<int>> & visited,int i,int j){
+    if(grid[i][j]==0||visited[i][j])return 0;
+    queue<pair<int,int>>q;
+    q.push({i,j});
+    visited[i][j]=1;
+    int cnt=0;
+    int m=grid.size(),n=grid[0].size();
+    while(!q.empty()){
+        int siz=q.size();
+        for(int k=0;k<siz;k++){
+            auto p=q.front();
+            q.pop();
+            int r=p.first,c=p.second;
+            if(grid[r][c]==1)cnt++;
+            if(r+1<m&&!visited[r+1][c]&&grid[r+1][c]==1){visited[r+1][c]=1;q.push({r+1,c});}
+            if(r-1>=0&&!visited[r-1][c]&&grid[r-1][c]==1){visited[r-1][c]=1;q.push({r-1,c});}
+            if(c+1<n&&!visited[r][c+1]&&grid[r][c+1]==1){visited[r][c+1]=1;q.push({r,c+1});}
+            if(c-1>=0&&!visited[r][c-1]&&grid[r][c-1]==1){visited[r][c-1]=1;q.push({r,c-1});}
+        }
+    }
+    return cnt;
 }
     int numEnclaves(vector<vector<int>>& grid) {
         int m = grid.size(),n = grid[0].size();
         int tot1 = 0;
         vector<vector<int>> visited(m,vector<int>(n,0));
+
+        //BOUNDARY TRAVERSAL DFS
         for(auto v:grid){
             for(int i:v){
                 if(i==1) tot1++;
@@ -23,25 +36,28 @@ int DFS(vector<vector<int>> & grid,vector<vector<int>> & visited,int i,int j){
         }
         if(m==1){
             for(int j=0;j<n;++j){
-                tot1 -=DFS(grid,visited,0,j);
+                tot1 -=BFS(grid,visited,0,j);
             }
         }
         if(n==1){
             for(int i=0;i<m;++i){
-                tot1 -=DFS(grid,visited,i,0);
+                tot1 -=BFS(grid,visited,i,0);
             }
         }
         for(int j=0;j<n;++j){
-            tot1 -=DFS(grid,visited,0,j);
+            tot1 -=BFS(grid,visited,0,j);
         }
+        // leave first row
         for(int i=1;i<m;++i){
-            tot1 -=DFS(grid,visited,i,n-1);
+            tot1 -=BFS(grid,visited,i,n-1);
         }
+        // leave last col
         for(int j=n-2;j>=0;--j){
-            tot1 -=DFS(grid,visited,m-1,j);
+            tot1 -=BFS(grid,visited,m-1,j);
         }
+        // leave last row
         for(int i=m-2;i>0;--i){
-            tot1 -=DFS(grid,visited,i,0);
+            tot1 -=BFS(grid,visited,i,0);
         }
 
         return tot1;
