08bbb6f41f31b11f47fa9a014dc5c63fd97b47ea
Ashutosh Behera <ashupoco66@gmail.com>
2026-07-25 11:34:53 +0530

    Time: 8 ms (21.95%) | Memory: 19.4 MB (6.69%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,62 +1,38 @@
 class Solution {
 public:
-// approach 1 - DFS TOPO SORT + cycle detection
-bool DFScycle(vector<vector<int>> & G,vector<int> & visited,vector<int> &pathvis,int node){
-      visited[node] = 1;
-      pathvis[node] = 1;
-      for(int i : G[node]){
-          if(!visited[i]){
-              if(DFScycle(G,visited,pathvis,i)) return true;
-              
-          }else if(pathvis[i]){
-              //seen this twice in current path
-              return true;
-          }
-      }
-      pathvis[node] = 0; //backtrack
-      
-      return false;
-  }
-  void DFSTopo(vector<vector<int>> & G, vector<int> & vis,stack<int> &st,int strt){
-      if(vis[strt]) return;
-      vis[strt] = 1;
-      for(int i:G[strt]){
-          DFSTopo(G,vis,st,i);
-      }
-      st.push(strt);
-  }
+// approach 2 - BFS using TOPO SORT KAHN's ALGO 
+
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        //CONCEPT - 1. check cycle 
-        //        - 2. if no cycle - give the TOPO SORT
-        vector<int> topo;
+        //CONCEPT - 1. is  topo size using kahn's == n (vertices) then DAG else cycle 
         int V = numCourses;
         vector<vector<int>> G(V);
         for(auto v:prerequisites){
             G[v[0]].push_back(v[1]);
         }
-        vector<int> visited(V,0);
-        vector<int> pathvis(V,0);
-    
-        bool checkCycle = false;
-
+        vector<int> Indeg(V,0);
+        for(auto v: G){
+            for(int i:v) Indeg[i]++;
+        }
+        
+        queue<int> q;
+        
         for(int i = 0;i<V;i++){
-            if(DFScycle(G,visited,pathvis,i)) {
-                checkCycle = true;
-                break;
+            if(Indeg[i]==0) q.push(i);
+        }
+        vector<int> topo;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+            for(int i:G[node]){
+                Indeg[i]--;
+                if(Indeg[i]==0) q.push(i);
             }
         }
-        if(checkCycle) return topo;
-        stack<int> st;
-        vector<int> vis(V,0);
-        for(int i = 0;i<V;i++){
-            DFSTopo(G,vis,st,i);
-        }
-        while(!st.empty()){
-            topo.push_back(st.top());
-            st.pop();
-        }
+        
         // topo keeps least dependent at last ... acc to ques we need it at first so just reverse the topo obtained
+        vector<int> ans;
         reverse(topo.begin(),topo.end());
-        return topo;
+        return (topo.size()==V) ? topo : ans; 
     }
 };
