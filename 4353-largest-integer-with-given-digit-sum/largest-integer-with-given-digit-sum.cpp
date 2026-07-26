12b2597aba562e063a314ddafc5f27fbd09c6323
Ashutosh Behera <ashupoco66@gmail.com>
2026-07-26 15:22:05 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (10.84%) - LeetSync

--- 4353-largest-integer-with-given-digit-sum/largest-integer-with-given-digit-sum.cpp
+++ 4353-largest-integer-with-given-digit-sum/largest-integer-with-given-digit-sum.cpp
@@ -1,14 +1,5 @@
 class Solution {
 public:
-pair<int,int> Dig(int n){
-    int sum = 0,dig = 0;
-    while(n){
-        sum+=n%10;
-        dig++;
-        n/=10;
-    }
-    return{dig,sum};
-}
     int largestInteger(int n, int s) {
         if(s>9*n) return -1;
         if(s==0) return 0;
