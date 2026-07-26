147bd866fc849f09c5459921bcd6397b5680c467
Ashutosh Behera <ashupoco66@gmail.com>
2026-07-26 15:21:56 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (10.84%) - LeetSync

--- 4353-largest-integer-with-given-digit-sum/largest-integer-with-given-digit-sum.cpp
+++ 4353-largest-integer-with-given-digit-sum/largest-integer-with-given-digit-sum.cpp
@@ -12,12 +12,24 @@ pair<int,int> Dig(int n){
     int largestInteger(int n, int s) {
         if(s>9*n) return -1;
         if(s==0) return 0;
-        int i = 1;
-        int ans = -1;
-        while(Dig(i).first<=n){
-            if(Dig(i).second==s) ans = i;
-            i++;
+        int i = 9;
+        int ans = 0;
+        vector<int> ansarr(n,0);
+        int idx = 0;
+        int sum = 0;
+        while(i>=0 && idx<n){
+            if(sum+i>s){
+                i--;
+            }else{
+                sum+=i;
+                ansarr[idx] = i;
+                idx++;
+            }
         }
+        for(int k:ansarr){
+            ans = ans*10 + k;
+        }
+        for(int m:ansarr) cout<<m<<" ";
         return ans;
     }
 };
