aed0659fc3b7e7da5faae1a7e6ae01010464ef9a
Ashutosh Behera <ashupoco66@gmail.com>
2026-07-26 16:14:07 +0530

    Time: 110 ms (36.24%) | Memory: 350.4 MB (41.96%) - LeetSync

--- 4338-aggregate-two-time-series/aggregate-two-time-series.cpp
+++ 4338-aggregate-two-time-series/aggregate-two-time-series.cpp
@@ -17,57 +17,29 @@ public:
     vector<vector<int>> aggregateTimeSeries(vector<vector<int>>& series1, vector<vector<int>>& series2) {
         int ptr1 = 0,ptr2 = 0;
         vector<vector<int>> agg;
-        while(ptr1 < series1.size() && ptr2<series2.size()){
+        while(ptr1<series1.size()||ptr2<series2.size()){
             int ts,val;
-            if(series1[ptr1][0]==series2[ptr2][0]){
+            if(ptr1<series1.size()&&ptr2<series2.size()&&series1[ptr1][0]==series2[ptr2][0]){
                 ts = series1[ptr1][0];
-                val = series1[ptr1][1] + series2[ptr2][1];
+                val = series1[ptr1][1]+series2[ptr2][1];
                 ptr1++;ptr2++;
-            }
-            else if(series1[ptr1][0]<series2[ptr2][0]){
+            }else if(ptr2==series2.size()||(ptr1<series1.size()&&series1[ptr1][0]<series2[ptr2][0])){
                 ts = series1[ptr1][0];
-                int idx2 = UBidx(series2,series1[ptr1][0]);
-                if(idx2!=-1){
-                    val = series1[ptr1][1] + series2[idx2][1];
+                if(ptr2<series2.size()){
+                    val = series1[ptr1][1]+series2[ptr2][1];
                 }else{
                     val = series1[ptr1][1];
                 }
                 ptr1++;
             }else{
                 ts = series2[ptr2][0];
-                int idx1 = UBidx(series1,series2[ptr2][0]);
-                if(idx1!=-1){
-                    val = series2[ptr2][1]+series1[idx1][1];
+                if(ptr1<series1.size()){
+                    val = series2[ptr2][1]+series1[ptr1][1];
                 }else{
                     val = series2[ptr2][1];
                 }
                 ptr2++;
             }
-            agg.push_back({ts,val});
-        }
-
-        while(ptr1<series1.size()){
-            int ts,val;
-            ts = series1[ptr1][0];
-            int idx2 = UBidx(series2,series1[ptr1][0]);
-            if(idx2!=-1){
-                val = series1[ptr1][1] + series2[idx2][1];
-            }else{
-                val = series1[ptr1][1];
-            }
-            ptr1++;
-            agg.push_back({ts,val});
-        }
-        while(ptr2<series2.size()){
-            int ts,val;
-            ts = series2[ptr2][0];
-            int idx1 = UBidx(series1,series2[ptr2][0]);
-            if(idx1!=-1){
-                val = series2[ptr2][1] + series1[idx1][1];
-            }else{
-                val = series2[ptr2][1];
-            }
-            ptr2++;
             agg.push_back({ts,val});
         }
         return agg;
