1637353b103aaefd382e57785e513aaebe5780d6
Ashutosh Behera <ashupoco66@gmail.com>
2026-07-31 19:54:32 +0530

    Time: 799 ms (12.31%) | Memory: 40.2 MB (8.76%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,6 +1,6 @@
 class Solution {
 private:
-    int BFS(string src,string dest,map<string,vector<string>> & G){
+     int BFS(string src,string dest,map<string,vector<string>>& G){
         int V = G.size();
         map<string,int> dist;
         for(auto p:G){
@@ -9,12 +9,12 @@ private:
         dist[src] = 0;
         queue<pair<string,int>> q;
         q.push({src,0});
-        //do a simple BFS 
         while(!q.empty()){
             auto p = q.front();
             q.pop();
             string root = p.first;
             int d = p.second;
+            if(G.find(root)==G.end()) continue;
             for(auto node:G[root]){
                 if(1+d < dist[node]){
                     dist[node] = 1+d;
@@ -23,13 +23,13 @@ private:
             }
         }
         return (dist.find(dest)!=dist.end()) ? dist[dest] : INT_MAX;
-    }
+     }
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
         bool check = false;
         for(auto w:wordList){
             if(w==endWord){
-                check  = true;
+                check = true;
                 break;
             }
         }
@@ -37,9 +37,9 @@ public:
         if(!check) return 0;
         map<string,vector<string>> G;
         for(int i = 0;i<V-1;i++){
-            string u  = wordList[i];
+            string u = wordList[i];
             for(int j = i+1;j<V;j++){
-                string v  = wordList[j];
+                string v = wordList[j];
                 int diff = 0;
                 for(int k = 0;k<v.size();k++){
                     if(u[k]!=v[k]) diff++;
@@ -58,19 +58,20 @@ public:
             }
             if(diff==1) sources.push_back(w);
         }
-
         int minladd = min(BFS(beginWord,endWord,G),INT_MAX);
-        int found = false;
+        bool found = false;
         if(minladd!=INT_MAX){
             found = true;
         }
-        if(!found)
-        for(string s:sources){
-            minladd = min(BFS(s,endWord,G),minladd);
+        if(!found){
+            for(string s:sources){
+                minladd = min(BFS(s,endWord,G),minladd);
+            }
+            if(minladd!=INT_MAX){
+                minladd++;
+            }
         }
-        if(!found) if(minladd!=INT_MAX) minladd++;
         cout<<minladd<<endl;
         return (minladd==INT_MAX) ? 0 : minladd+1 ;
-
     }
 };
