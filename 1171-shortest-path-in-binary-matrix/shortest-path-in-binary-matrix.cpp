898bef8a6dd9619f4e56d08ccff57a3e670f0832
Ashutosh Behera <ashupoco66@gmail.com>
2026-08-01 22:55:15 +0530

    Time: 11 ms (88.61%) | Memory: 23.4 MB (92.79%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -2,33 +2,35 @@ class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n = grid.size();
-        vector<vector<int>> dist(n,vector<int> (n,INT_MAX));
-        if(n==1) return (grid[0][0]) ? -1:1;
-        if(grid[0][0]) return -1;
-        set<pair<int,pair<int,int>>> st;
-        st.insert({!grid[0][0],{0,0}});
-        while(!st.empty()){
-            auto [d1,p] = *st.begin();
-            auto [i,j] = p;
-            st.erase(st.begin());
-            if(d1>dist[i][j]) continue;
-            int drow[] = {-1,0,1};
-            int dcol[] = {-1,0,1};
-            for(int r :drow){
-                for(int c:dcol){
-                    int row = r+i,col = c+j;
-                    if(row>=0 && col>=0 && row<n && col<n){
-                        if(row==i && col==j) continue;
-                        if(d1+!grid[row][col] < dist[row][col] && grid[row][col]==0){
-                            dist[row][col] = d1+!grid[row][col];
-                            st.insert({dist[row][col],{row,col}});
-                        }
+        if (grid[0][0] == 1 || grid[n-1][n-1] == 1) return -1;
+        if (n == 1) return 1;
+        queue<pair<int, int>> q;
+        q.push({0, 0});
+        grid[0][0] = 1;
+        int pathLength = 1;
+
+        int drow[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+        int dcol[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+        while (!q.empty()) {
+            int size = q.size();
+            while (size--) {
+                auto [r, c] = q.front();
+                q.pop();
+                if (r == n - 1 && c == n - 1) return pathLength;
+                for (int i = 0; i < 8; i++) {
+                    int nr = r + drow[i];
+                    int nc = c + dcol[i];
+
+                    if (nr >= 0 && nc >= 0 && nr < n && nc < n && grid[nr][nc] == 0) {
+                        grid[nr][nc] = 1;
+                        q.push({nr, nc});
                     }
                 }
             }
-            
+            pathLength++;
         }
-        return (dist[n-1][n-1]==INT_MAX) ? -1:dist[n-1][n-1];
+        return -1;
 
     }
 };
