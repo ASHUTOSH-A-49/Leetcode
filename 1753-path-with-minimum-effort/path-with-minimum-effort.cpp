d8d0ffcde3fbc8af5ad0857bd9c7721dded4a378
Ashutosh Behera <ashupoco66@gmail.com>
2026-08-02 18:19:52 +0530

    Time: 53 ms (60.90%) | Memory: 24.5 MB (77.32%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -5,10 +5,7 @@ public:
 
         int n = heights.size(), m = heights[0].size(); 
         vector<vector<int>> dist(n, vector<int>(m, 1e7)); 
-        vector<vector<pair<int,int>>> parent(n, vector<pair<int,int>>(m)); 
-        
-        dist[0][0] = 0; 
-        parent[0][0] = {0,0}; 
+        dist[0][0] = 0;
         
         priority_queue<pair<int, pair<int, int>>, 
                        vector<pair<int, pair<int, int>>>, 
@@ -35,39 +32,13 @@ public:
                     
                     if(d2 < dist[nr][nc]) { 
                         dist[nr][nc] = d2; 
-                        parent[nr][nc] = {r, c}; 
                         st.push({d2, {nr, nc}}); 
                     } 
                 } 
             } 
         } 
         
-        for(int i = 0; i < n; i++){ 
-            for(int j = 0; j < m; j++){ 
-                auto p = parent[i][j]; 
-                cout << p.first << " " << p.second << "\t"; 
-            } 
-            cout << endl; 
-        } 
-        
-        int maxd = 0; 
-        int r1 = n-1, c1 = m-1; 
-        
-        if (n == 1 && m == 1) return 0; 
-        
-        while(true){ 
-            auto p = parent[r1][c1]; 
-            int r2 = p.first, c2 = p.second; 
-            
-            maxd = max(maxd, abs(heights[r1][c1] - heights[r2][c2])); 
-            
-            if(r2 == 0 && c2 == 0){ 
-                break; 
-            } 
-            r1 = r2; 
-            c1 = c2; 
-        } 
-        return maxd;
+        return dist[n-1][m-1];
 
     }
 };
