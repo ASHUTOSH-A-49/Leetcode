359eff8f5e3f703d5b7024c02f5c261d40d84628
Ashutosh Behera <ashupoco66@gmail.com>
2026-08-04 00:43:09 +0530

    Time: 4 ms (45.21%) | Memory: 18.4 MB (37.75%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,36 +1,33 @@
 class Solution {
 public:
-// DIJKSTRA APPROACH 
+// BFS approach
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
         vector<vector<pair<int,int>>> G(n);
         for(auto f:flights){
             int u = f[0],v = f[1],p = f[2];
             G[u].push_back({p,v});
         }
-        set<pair<int,pair<int,int>>> st;
+        queue<pair<int,pair<int,int>>> q;
+        vector<int> dist(n,1e9);
+        dist[src] = 0;
+        //queue by stops q = {stops,{node,dist}};
+        q.push({0,{src,0}});
         k++;
-        vector<vector<int>> dist(n,vector<int>(k+2,1e9));
-        //sort by price
-        st.insert({0,{0,src}});
-        dist[src][0]=0;
-        while(!st.empty()){
-            auto p = *st.begin();
-            int d = p.first; 
-            auto [stop,node] = p.second;
-            st.erase(st.begin());
-            if(node==dst)return d;
+        while(!q.empty()){
+            auto p = q.front();
+            q.pop();
+            int stop = p.first;
+            auto [node,d] = p.second;
             for(auto [d2,i]:G[node]){
-                //CORE LOGIC: only proceed if within k allowed stops
                 if(stop+1<=k){
-                    if(d+d2<dist[i][stop+1]){
-                        dist[i][stop+1]=d+d2;
-                        st.insert({d+d2,{stop+1,i}});
+                    if(d+d2<dist[i]){
+                        dist[i] = d+d2;
+                        q.push({stop+1,{i,dist[i]}});
                     }
                 }
             }
         }
-
-        return -1;
+        return (dist[dst]==1e9) ? -1:dist[dst];
         
     }
 };
