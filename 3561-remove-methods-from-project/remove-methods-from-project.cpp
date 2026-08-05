cf969736e54e768889cbcfd5389b7937678f15e7
Ashutosh Behera <ashupoco66@gmail.com>
2026-08-05 18:35:46 +0530

    Time: 1466 ms (5.17%) | Memory: 332 MB (42.07%) - LeetSync

---
+++ 3561-remove-methods-from-project/remove-methods-from-project.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
+        vector<int> ans;
+        vector<vector<int>> G(n);
+        for(auto i:invocations){
+            int u = i[0],v = i[1];
+            G[u].push_back(v);
+        }
+        vector<bool> isSusp(n,false);
+        isSusp[k] = true;
+        //BFS from suspicious to get al suspicious
+        queue<int> q;
+        q.push(k);
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            for(int i:G[node]){
+                if(isSusp[i]) continue;
+                q.push(i);
+                isSusp[i] = true;
+            }
+        }
+        for(bool b:isSusp) cout<<b<<" ";
+        bool check = false;
+        for(int i = 0;i<n;i++){
+            if(isSusp[i]) continue;
+            for(int u:G[i]){
+                if(isSusp[u]) {
+                    check = true;
+                    break;
+                }
+            }
+        }
+        if(check){
+            for(int i = 0;i<n;i++) ans.push_back(i);
+        }else{
+            for(int i = 0;i<n;i++){
+                if(!isSusp[i]) ans.push_back(i);
+            }
+        }
+
+        return ans;
+    }
+};
