92b0603d68de2f1de7910a644d6185143dd7a38e
Ashutosh Behera <ashupoco66@gmail.com>
2026-08-05 19:01:24 +0530

    Time: 157 ms (87.93%) | Memory: 303.1 MB (76.21%) - LeetSync

--- 3561-remove-methods-from-project/remove-methods-from-project.cpp
+++ 3561-remove-methods-from-project/remove-methods-from-project.cpp
@@ -2,34 +2,36 @@ class Solution {
 public:
 
     vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         vector<int> ans;
         vector<vector<int>> G(n);
-        for(auto i:invocations){
-            int u = i[0],v = i[1];
+        
+        for(const auto& i : invocations){
+            int u = i[0], v = i[1];
             G[u].push_back(v);
         }
-        vector<bool> isSusp(n,false);
+        vector<bool> isSusp(n, false);
         isSusp[k] = true;
-        //BFS from suspicious to get al suspicious
         queue<int> q;
         q.push(k);
         while(!q.empty()){
             int node = q.front();
             q.pop();
-            for(int i:G[node]){
+            for(int i : G[node]){
                 if(isSusp[i]) continue;
                 q.push(i);
                 isSusp[i] = true;
             }
         }
-        // for(bool b:isSusp) cout<<b<<" ";
-        // bool check = false;
-        vector<int> visited(n,0);
-        bool check  = false;
+        
+        vector<int> visited(n, 0);
+        bool check = false;
         for(int i = 0; i < n; i++){
             if(isSusp[i]) continue;
             if(visited[i]) continue;
-            visited[i] = 1; 
+            visited[i] = 1;
+            
             for(int u : G[i]){
                 if(isSusp[u]) {
                     check = true;
@@ -38,14 +40,14 @@ public:
             }
             if(check) break;
         }
+        
         if(check){
-            for(int i = 0;i<n;i++) ans.push_back(i);
-        }else{
-            for(int i = 0;i<n;i++){
+            for(int i = 0; i < n; i++) ans.push_back(i);
+        } else {
+            for(int i = 0; i < n; i++){
                 if(!isSusp[i]) ans.push_back(i);
             }
         }
-
         return ans;
     }
 };
