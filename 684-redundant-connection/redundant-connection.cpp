eaf928382695315de9b371f2516472a825d6dd4b
Ashutosh Behera <ashupoco66@gmail.com>
2026-08-06 20:51:02 +0530

    Time: 1 ms (39.21%) | Memory: 13.5 MB (36.44%) - LeetSync

---
+++ 684-redundant-connection/redundant-connection.cpp
@@ -0,0 +1,69 @@
+class DSU{
+    vector<int> rank,size,par;
+public:
+    DSU(int n){
+        rank.assign(n+1,0);
+        size.assign(n+1,0);
+        par.assign(n+1,0);
+        for(int i = 1;i<=n;i++) par[i] = i; //1 based
+        
+
+    }
+
+    int findUpar(int u){
+        if(par[u]==u) return u;
+        return par[u] = findUpar(par[u]); // PATH COMPRESSION
+    }
+
+    void unionByRank(int u,int v){
+        int ult_pu = findUpar(u);
+        int ult_pv = findUpar(v);
+        if(ult_pu==ult_pv) return;
+        if(rank[ult_pu] < rank[ult_pv]){
+            par[ult_pu] = ult_pv; 
+        }else if(rank[ult_pu] > rank[ult_pv]){
+            par[ult_pv] = ult_pu;
+        }else{
+            par[ult_pv] = ult_pu;
+            //if equal attach v to u and as they were equal new rank increments by  aafter attaching (DRY RUN TO UNDERSTAND)
+            rank[ult_pu]++;
+        }
+    }
+    void unionBySize(int u,int v){
+        int ult_pu = findUpar(u);
+        int ult_pv = findUpar(v);
+        if(ult_pu==ult_pv) return;
+        if(size[ult_pu] < size[ult_pv]){
+            par[ult_pu] = ult_pv; 
+        }else if(size[ult_pu] > size[ult_pv]){
+            par[ult_pv] = ult_pu;
+        }else{
+            par[ult_pv] = ult_pu;
+            //if equal attach v to u and as they were equal new rank increments by  aafter attaching (DRY RUN TO UNDERSTAND)
+            size[ult_pu]+=size[ult_pv];
+        }
+    }
+
+    bool checkComp(int u,int v){
+        return (findUpar(u)==findUpar(v));
+    }
+        
+};
+
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n = edges.size();
+        vector<int> ans;
+        DSU ds(n);
+        for(auto e:edges){
+            int u = e[0],v = e[1];
+            if(ds.findUpar(u)==ds.findUpar(v)){
+                ans = {u,v};
+            }
+            ds.unionBySize(u,v);
+        }
+        return ans;
+
+    }
+};
