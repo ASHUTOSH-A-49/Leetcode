797a6051e1150c822a3a54dea31146299165cb32
Ashutosh Behera <ashupoco66@gmail.com>
2026-08-09 00:26:46 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (88.73%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,17 +1,6 @@
 class Solution {
 public:
-// int dp[50];
-// //memoization (top-down)
-//     int solve(int i,int n){
-//         if(dp[i]!=-1) return dp[i];
-//         if(i>n) return 0;
-//         if(i==n){
-//             return 1;
-//         }
-//         return dp[i] = solve(i+1,n) +
-//         solve(i+2,n);
 
-//     }
     int climbStairs(int n) {
         //tabulation  (bottom up)
         int dp[n+1];
