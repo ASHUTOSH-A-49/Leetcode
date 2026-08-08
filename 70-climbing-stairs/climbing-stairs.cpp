be890690d66f498fe442f2c140174f97751330f5
Ashutosh Behera <ashupoco66@gmail.com>
2026-08-09 00:26:36 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (88.73%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
-int dp[50];
-//memoization (top-down)
-    int solve(int i,int n){
-        if(dp[i]!=-1) return dp[i];
-        if(i>n) return 0;
-        if(i==n){
-            return 1;
-        }
-        return dp[i] = solve(i+1,n) +
-        solve(i+2,n);
+// int dp[50];
+// //memoization (top-down)
+//     int solve(int i,int n){
+//         if(dp[i]!=-1) return dp[i];
+//         if(i>n) return 0;
+//         if(i==n){
+//             return 1;
+//         }
+//         return dp[i] = solve(i+1,n) +
+//         solve(i+2,n);
 
-    }
+//     }
     int climbStairs(int n) {
-        memset(dp,-1,sizeof(dp));
-        return solve(0,n);
+        //tabulation  (bottom up)
+        int dp[n+1];
+        dp[n] = 1;
+        dp[n-1] = 2;
+        for(int i = n-2;i>0;i--){
+            dp[i] = dp[i+1]+dp[i+2];
+        }
+        return dp[1];
         
     }
 };
