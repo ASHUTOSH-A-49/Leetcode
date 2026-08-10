64b6f0471424ad871823c0bac5bc25c399197359
Ashutosh Behera <ashupoco66@gmail.com>
2026-08-11 00:37:30 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (6.15%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
-    //tabulation approach
+    //tabulation approach (space optimised)
     int rob(vector<int>& nums) {
         int n = nums.size();
         vector<int> dp(n,-1);
         if(n==1) return nums[0];
-        dp[0] = nums[0];
-        dp[1]= max(nums[0],nums[1]);
+        int t2 = nums[0];
+        int t1= max(nums[0],nums[1]);
         for(int i = 2;i<n;i++){
-            dp[i] = max(dp[i-1],dp[i-2]+nums[i]);
+            int curr = max(t1,t2+nums[i]);
+            // if robbed i-1 th value then dont rob curr house else rob current house
+            t2 = t1;
+            t1 = curr;
         }
-        return dp[n-1];
+        return t1;
     }
 };
