a700f28b632a4485704f9fdbe3548be22635688d
Ashutosh Behera <ashupoco66@gmail.com>
2026-08-10 23:43:06 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (65.12%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int arob(int i,vector<int>& nums,vector<int> & memo){
-        if(i<0) return 0;
+        int n= nums.size();
+        if(i>=n) return 0;
         if(memo[i]!=-1){
             return memo[i];
         }
-        int pick = nums[i] + arob(i - 2, nums, memo);
-        int skip = 0 + arob(i - 1, nums, memo);
+        int pick = nums[i] + arob(i + 2, nums, memo);
+        int skip = 0 + arob(i + 1, nums, memo);
         return memo[i] = max(pick, skip);
         
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
         vector<int> memo(n+1,-1);
-        return arob(n-1,nums,memo);
+        return arob(0,nums,memo);
     }
 };
