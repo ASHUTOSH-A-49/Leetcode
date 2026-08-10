7a03811200a2176dcc04ff407b014fe4e0c5e2cd
Ashutosh Behera <ashupoco66@gmail.com>
2026-08-10 16:23:39 +0530

    Time: 114 ms (23.71%) | Memory: 8.9 MB (82.55%) - LeetSync

---
+++ 1617-stone-game-iv/stone-game-iv.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool winnerSquareGame(int n) {
+        vector<bool> dp(n+1,false);
+        dp[1] = true;
+        //dp[i] = is ith state a winning state? i.e. if player starts with i stones and they play can player win ?
+        for(int i = 2;i<=n;i++){
+            for(int j = 1;j*j<=i;j++){
+                int sq = j*j;
+                // if after removing sq stones, the state ends in a losing state then we declare current state to be winning state
+                if(!dp[i-sq]) dp[i] = true;
+            }
+        }
+        return dp[n];
+    }
+};
