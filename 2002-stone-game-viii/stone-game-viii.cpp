ccf477957f690e89395fe8e55165143e36dd380b
Ashutosh Behera <ashupoco66@gmail.com>
2026-08-24 21:12:51 +0530

    Time: 555 ms (5.13%) | Memory: 111.3 MB (5.57%) - LeetSync

---
+++ 2002-stone-game-viii/stone-game-viii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+int dp[1000001];
+int solve(int i, vector<int> & pre){
+    int n = pre.size();
+    if(i==n-1) return pre[i];
+    if(dp[i]!=-1) return dp[i];
+    int take = pre[i]-solve(i+1,pre);
+    int skip = solve(i+1,pre);
+
+    return dp[i] = max(take,skip);
+}
+    int stoneGameVIII(vector<int>& stones) {
+        
+        memset(dp,-1,sizeof(dp));
+        int n = stones.size();
+        vector<int> nums;
+        for(int i = 0;i<n;i++){
+            if(stones[i]!=0) nums.push_back(stones[i]);
+        }
+        for(int i:nums) cout<<i<<" ";
+        cout<<endl;
+        int n1 = nums.size();
+        vector<int> pre(n1);
+        int sum =0;
+        for(int i = 0;i<n1;i++){
+            sum+=nums[i];
+            pre[i] = sum;
+        }
+        for(int i:pre) cout<<i<<" ";
+        return (nums.size()>1) ? solve(1,pre) : ((nums.size()>0)?nums[0]:0);
+    }
+};
