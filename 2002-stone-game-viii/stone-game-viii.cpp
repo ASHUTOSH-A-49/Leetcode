34720636bf22c2fe3aa6b037a2e6b8aa1662c13a
Ashutosh Behera <ashupoco66@gmail.com>
2026-08-24 21:28:07 +0530

    Time: 67 ms (99.27%) | Memory: 101.2 MB (27.82%) - LeetSync

--- 2002-stone-game-viii/stone-game-viii.cpp
+++ 2002-stone-game-viii/stone-game-viii.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-int dp[1000001];
-int solve(int i, vector<int> & pre,int n){
-    if(i==n-1) return pre[i];
-    if(dp[i]!=-1) return dp[i];
-    int take = pre[i]-solve(i+1,pre,n);
-    int skip = solve(i+1,pre,n);
+// int dp[1000001];
+// int solve(int i, vector<int> & pre,int n){
+//     if(i==n-1) return pre[i];
+//     if(dp[i]!=-1) return dp[i];
+//     int take = pre[i]-solve(i+1,pre,n);
+//     int skip = solve(i+1,pre,n);
 
-    return dp[i] = max(take,skip);
-}
+//     return dp[i] = max(take,skip);
+// }
     int stoneGameVIII(vector<int>& stones) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
-        memset(dp,-1,sizeof(dp));
+        // memset(dp,-1,sizeof(dp));
         int n = stones.size();
         vector<int> nums;
         for(int i = 0;i<n;i++){
             if(stones[i]!=0) nums.push_back(stones[i]);
         }
         // for(int i:nums) cout<<i<<" ";
-        cout<<endl;
+        // cout<<endl;
         int n1 = nums.size();
         vector<int> pre(n1);
         int sum =0;
@@ -28,6 +28,15 @@ int solve(int i, vector<int> & pre,int n){
             pre[i] = sum;
         }
         // for(int i:pre) cout<<i<<" ";
-        return (nums.size()>1) ? solve(1,pre,(int)pre.size()) : ((nums.size()>0)?nums[0]:0);
+        if(nums.size()==0) return 0;
+        if(nums.size()==1) return nums[0];
+
+        int dp[n1];
+        dp[n1-1] = pre[n1-1];
+        for(int i = n1-2;i>0;i--){
+            dp[i] = max(dp[i+1],pre[i]-dp[i+1]);
+        }
+        return dp[1];
+
     }
 };
