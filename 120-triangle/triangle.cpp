7ed66bbfb57eb444e739bf89eadf80b19cebc048
Ashutosh Behera <ashupoco66@gmail.com>
2026-08-25 15:10:42 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (59.36%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
-int dp[202][202];
-    int solve(int i,int j,vector<vector<int>>& triangle,int n){
-        if(i==n-1) return triangle[i][j];
-        if(dp[i][j]!=INT_MAX) return dp[i][j];
-        int take1 = solve(i+1,j,triangle,n);
-        int take2 = solve(i+1,j+1,triangle,n);
-        return dp[i][j] = triangle[i][j]+min(take1,take2);
-    }
+// int dp[202][202];
+//     int solve(int i,int j,vector<vector<int>>& triangle,int n){
+//         if(i==n-1) return triangle[i][j];
+//         if(dp[i][j]!=INT_MAX) return dp[i][j];
+//         int take1 = solve(i+1,j,triangle,n);
+//         int take2 = solve(i+1,j+1,triangle,n);
+//         return dp[i][j] = triangle[i][j]+min(take1,take2);
+//     }
     int minimumTotal(vector<vector<int>>& triangle) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
         int n = triangle.size();
+        vector<int> dp(n);
         for(int i = 0;i<n;i++){
-            for(int j = 0;j<n;j++){
-                dp[i][j] = INT_MAX;
-            }
+            dp[i] = triangle[n-1][i];
         }
-        
-        return solve(0,0,triangle,n);
+        int siz = n-2;
+        for(int i = n-2;i>=0;i--){
+            vector<int> temp(dp.begin(),dp.begin()+siz+2);
+            for(int j = siz;j>=0;j--){
+                dp[j] = triangle[i][j]+min(temp[j],temp[j+1]);
+            }
+            siz--;
+        }
+        return dp[0];
     }
 };
