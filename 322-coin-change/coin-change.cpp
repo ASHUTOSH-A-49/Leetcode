c0d588648975b17139a53d108ed3ddb0dbf112d6
Ashutosh Behera <ashupoco66@gmail.com>
2026-09-04 23:57:10 +0530

    Time: 31 ms (58.97%) | Memory: 13.7 MB (96.56%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int amount) {
-        if(amount == 0) return 0;
-        sort(coins.begin(),coins.end());
-        vector<int> C(amount+1,INT_MAX);
-        C[0] = 0;
-        for(int i = 1;i<=amount;i++){
-            int res = INT_MAX;
-            for(int c:coins){
-                if(c>i) break;
-                // cout<<"enter "<<i<<endl;
-                res = min(res,C[i-c]);
-            }
-
-            if(res!=INT_MAX) C[i] = 1+res;
-            
+int dp[12][10001];
+    int solve(int idx,vector<int> & coins, int target, int &n){
+        if(idx==n){
+            return (target==0)? 0 : 1e6;
         }
-        // for(int i:C) cout<<i<<" ";
-
-        return (C[amount]==INT_MAX) ? -1: C[amount];
+        if(target<0) return 1e6;
+        if(dp[idx][target]!=-1) return dp[idx][target];
+        int take = 1+solve(idx,coins,target-coins[idx],n);
+        int skip = solve(idx+1,coins,target,n);
+        return dp[idx][target]=min(take,skip);
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        memset(dp,-1,sizeof(dp));
+        int n = coins.size();
+       int res= solve(0,coins,amount,n);
+       return (res==1e6) ? -1:res;
 
     }
 };
