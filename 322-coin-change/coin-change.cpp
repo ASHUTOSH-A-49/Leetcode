4076344f77a7faeba103956eea8f22dd94e87a94
Ashutosh Behera <ashupoco66@gmail.com>
2026-09-07 00:17:47 +0530

    Time: 21 ms (71.97%) | Memory: 17.9 MB (84.01%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
-//TABULATION (BOTTOM - UP)
+//TABULATION (BOTTOM - UP) with SPACE OPTIMIZ'n
     int coinChange(vector<int>& coins, int amount) {
        int n = coins.size();
-
-       vector<vector<int>> dp(n+1,vector<int>(amount+1,1e6));
+       vector<int> dp(amount+1,1e6);
        //base case;
-       dp[n][0] = 0;
+       dp[0] = 0;
        for(int idx = n-1;idx>=0;idx--){
         for(int t = 0;t<=amount;t++){
-            int skip = dp[idx+1][t];
+            int skip = dp[t];
             int take = 1e6;
-            if(t>=coins[idx]) take =1+ dp[idx][t-coins[idx]];
-            dp[idx][t] = min(take,skip);
+            if(t>=coins[idx]) take =1+ dp[t-coins[idx]];
+            dp[t] = min(take,skip);
         }
        }
-       return (dp[0][amount]>=1e6)?-1 : dp[0][amount];
+       return (dp[amount]>=1e6)?-1 : dp[amount];
 
     }
 };
