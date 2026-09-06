2ac21cc6f239e3027350de706aca1ad40ce05dfd
Ashutosh Behera <ashupoco66@gmail.com>
2026-09-07 02:13:34 +0530

    Time: 35 ms (42.76%) | Memory: 36.5 MB (64.25%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-int dp[301][5001];
-    int solve(int idx,vector<int> & coins, int target, int &n){
-        if(target<0) return 0;
-        if(idx==n){
-            return (target==0)? 1 : 0;
-        }
-        if(target<0) return 1e6;
-        if(dp[idx][target]!=-1) return dp[idx][target];
-        int take = solve(idx,coins,target-coins[idx],n);
-        int skip = solve(idx+1,coins,target,n);
-        return dp[idx][target]=take+skip;
-    }
+//TABULATION (BOTTOM - UP)
     int change(int amount, vector<int>& coins) {
-        memset(dp,-1,sizeof(dp));
         int n = coins.size();
-       int res= solve(0,coins,amount,n);
-       return res;
+        vector<vector<unsigned int>> dp(n+1,vector<unsigned int>(amount+1,0));
+        //base cases
+        for(int i = 0;i<=n;i++)
+        dp[i][0] = 1;
+        for(int idx = n-1;idx>=0;idx--){
+            for(int a = 0;a<=amount;a++){
+                unsigned int take = 0;
+                unsigned int skip = dp[idx+1][a];
+                if(a>=coins[idx]) take = dp[idx][a-coins[idx]];
+                dp[idx][a] = take+skip;
+            }
+        }
+        return dp[0][amount];
 
     }
 };
